6dfcef82eb9af64dfc65f3da5df405f3041ff7eb
faizan <faizan89ahmad@gmail.com>
2022-08-20 22:07:54 +0530

    mernapp

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.use("/products", router);
 
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static("ecommerce/build"))
+    app.use(express.static("client/build"))
     const path = require("path")
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"ecommerce",'build','index.html'))
+        res.sendFile(path.resolve(__dirname,"client",'build','index.html'))
     })
 }
 
